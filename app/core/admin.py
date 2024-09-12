26fabe4473225bbaac639ac03627731b24609b95
Nathaniel Allen <nathaniel.allen@unique-landuse.de>
2024-09-12 14:29:43 -0700

    finish base admin config

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,44 @@
-from django.contrib import admin # noqa
+"""
+Django admin customization
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# _ is considered django/python? shorthand to access translation model
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    # first field is a title for grouping the fields
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Permissions'),
+         {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+    readonly_fields = ['last_login']
+
+    # For creating new objects; 'classes' is to use a custom css tag
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
+
+
+# Passing in UserAdmin allow us to override defaults, e.g. custom sorting,
+#  disabling CRUD, etc.
+admin.site.register(models.User, UserAdmin)
