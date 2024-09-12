2cc61695873efb8ff6be0c8899b69761c6742391
Nathaniel Allen <nathaniel.allen@unique-landuse.de>
2024-09-12 13:15:38 -0700

    normalize emails

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -19,3 +19,18 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+        # list of lists
+        sample_emails = (
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com']
+        )
+
+        # loop through list extracting multiple elements in sub-ist
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
