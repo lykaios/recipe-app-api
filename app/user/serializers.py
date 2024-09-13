e9e46c2240d241188c25b2172ccd55af7f6dedda
Nathaniel Allen <nathaniel.allen@unique-landuse.de>
2024-09-13 11:12:16 -0700

    add user token-based auth

---
+++ app/user/serializers.py
@@ -0,0 +1,55 @@
+"""
+Serializers for the user API View.
+
+A serialiser is a Django model for converting JSON->Objects, incl. validation
+"""
+from django.contrib.auth import (
+    get_user_model,
+    authenticate,
+)
+from django.utils.translation import gettext as _
+
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the user object."""
+
+    class Meta:
+        model = get_user_model()
+        fields = ['email', 'password', 'name']
+        # kwargs is dict with extra attributes for fields, e.g. cant read
+        #  passwords in API
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+    # overrides default behavior
+    def create(self, validated_data):
+        """Create and return a user with encrypted password."""
+        return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token."""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user."""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        # built-in django method; little weird but requires request even though
+        #  its not clear what it is used for in this context
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+        if not user:
+            msg = _('Unable to authenticate with provided credentials.')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
