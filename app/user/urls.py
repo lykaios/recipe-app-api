efb6b6f0845129e8860d74f79578e7ac0a74cf6f
Nathaniel Allen <nathaniel.allen@unique-landuse.de>
2024-09-13 11:54:09 -0700

    testing in swagger

--- app/user/urls.py
+++ app/user/urls.py
@@ -8,7 +8,9 @@ from user import views
 
 app_name = 'user'
 
+# me route is to update a signed-in user (ie self)
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
-    path('token/', views.CreateTokenView.as_view(), name='token')
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('me/', views.ManageUserView.as_view(), name='me')
 ]
